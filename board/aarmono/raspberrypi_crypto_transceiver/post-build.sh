80e5eab90e387618fa5a6a3213595b31235b49eb
aarmono <crypto@aarmono.com>
2023-08-10 12:04:41 -0700

    Remove pppoe.conf from /etc/ppp

--- board/aarmono/raspberrypi_crypto_transceiver/post-build.sh
+++ board/aarmono/raspberrypi_crypto_transceiver/post-build.sh
@@ -4,3 +4,8 @@ if test -e "${TARGET_DIR}/etc/init.d/S50sshd"
 then
 	mv "${TARGET_DIR}/etc/init.d/S50sshd" "${TARGET_DIR}/etc/init.d/manual/"
 fi
+
+if test -e "${TARGET_DIR}/etc/ppp/pppoe.conf"
+then
+	rm "${TARGET_DIR}/etc/ppp/pppoe.conf"
+fi
